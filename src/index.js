b4df89880a778a8a1127697fe8bab91e0951139d
Rodrigo Rossi <rdrg.rossi@gmail.com>
2021-05-04 10:57:33 -0300

    env config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+console.log('Deu boa');
 
-index > 
-    routes > [ API Publica ]
-    routes > [ API Privada ]
-        controller > [ Pré Validações ] (model)
-            service > [ Especialista em regra de BD ]
-                repository > [ Escrever no BD Mongo    ]
-                repository > [ Escrever no BD Postgres ]            
+
+
+
+
+
+console.log(process.env.DB_URL);
