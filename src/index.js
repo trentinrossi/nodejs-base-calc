84e1655d1a45548ed98a8598628b4d10a9d0b7ca
Rodrigo Rossi <rdrg.rossi@gmail.com>
2021-05-04 10:42:07 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+index > 
+    routes > [ API Publica ]
+    routes > [ API Privada ]
+        controller > [ Pré Validações ] (model)
+            service > [ Especialista em regra de BD ]
+                repository > [ Escrever no BD Mongo    ]
+                repository > [ Escrever no BD Postgres ]            
